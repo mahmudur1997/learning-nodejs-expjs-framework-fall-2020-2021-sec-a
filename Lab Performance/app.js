bce411959eab7f782d8b73a999a51905d434e922
mahmudur1997 <44579596+mahmudur1997@users.noreply.github.com>
2020-11-10 13:01:39 +0600

    initial

---
+++ Lab Performance/app.js
@@ -0,0 +1,40 @@
+//declaration
+const express 			= require('express');	
+const bodyParser 		= require('body-parser');
+const exSession 		= require('express-session');
+const cookieParser 		= require('cookie-parser');
+
+const login				= require('./controllers/login');
+const logout			= require('./controllers/logout');
+const admin_home		= require('./controllers/Admin_home');
+const employee_home		= require('./controllers/Employee_home');
+const user				= require('./controllers/user');
+const app				= express();
+const port				= 3000;
+
+//configuration
+app.set('view engine', 'ejs');
+
+
+//middleware
+app.use('/abc', express.static('assets'))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(exSession({secret: 'secret value', saveUninitialized: true, resave: false}));
+
+
+app.use('/login', login);
+app.use('/Admin_home', admin_home);
+app.use('Employee_home',employee_home);
+app.use('/logout', logout);
+app.use('/user', user);
+
+//router
+app.get('/', (req, res)=>{
+	res.send('Welcome');
+});
+
+//server startup
+app.listen(port, (error)=>{
+	console.log('server strated at '+port);
+});
---
+++ Lab Performance/controllers/login.js
@@ -0,0 +1,31 @@
+const express 		= require('express');
+const userModel		= require.main.require('./models/userModel');
+const router 		= express.Router();
+
+router.get('/', (req, res)=>{
+	res.render('login/index');
+});
+
+router.post('/', (req, res)=>{
+
+	var user = {
+		username: req.body.username,
+		password: req.body.password
+	};
+
+	userModel.validate(user, function(status){
+		if(status == 0){
+			res.cookie('uname', req.body.username);
+			res.redirect('/Admin_home');
+		}
+		else if(status == 1){
+			res.cookie('uname', req.body.username);
+			res.redirect('/Admin_employee');
+		}
+		else{
+			res.redirect('/login');
+		}
+	});
+}); 
+
+module.exports = router;
